7e60b2969e82dd628752fdc88c337d6ca62ab74f
Umut Özkan <umuttozkannis@gmail.com>
2023-06-27 12:21:27 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from plyer import notification
+alarm_title="Uyarı!!!!!!!!!!!!!!"
+alarm_message="Deneme"
+notification.notify(title=alarm_title,message=alarm_message,app_icon="picture1.ico",timeout=50,toast=False)
